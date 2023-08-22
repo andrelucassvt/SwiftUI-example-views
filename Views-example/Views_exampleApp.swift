d8e69c54ee0c7b6d09aef9a632db04ba1f1c75cd
andresalvador-vwi <58487111+andrelucassvt@users.noreply.github.com>
2023-08-22 09:19:18 -0300

    Initial Commit

---
+++ Views-example/Views_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Views_exampleApp.swift
+//  Views-example
+//
+//  Created by André  Lucas on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Views_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
